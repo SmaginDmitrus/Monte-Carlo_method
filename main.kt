c655672e6801510de5cf4434f690d3dab781edab
SmaginDmitrus <74429578+SmaginDmitrus@users.noreply.github.com>
2021-11-09 09:16:46 +0300

    Update main.kt

--- main.kt
+++ main.kt
@@ -3,10 +3,11 @@ class Dot<A>(vararg coordinate: A ){
     init{
     for (i in coordinate) coordinates.add(i)
     }
+    operator fun get(index:Int):A{ return coordinates[index]}
    
 }
 fun main() {
     var x = Dot(1.0,1.0)
-    var sum = x.coordinates[0] + x.coordinates[1]
+    var sum = x[0] + x[1]
     print(sum)
 }
